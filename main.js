f54eadbe321c75ccd365190c7e23ddd942ce8a88
Ryan Jowers <rjowers@g.clemson.edu>
2017-02-03 13:48:30 -0500

    finished question 1

--- main.js
+++ main.js
@@ -2,13 +2,30 @@
 
 function find() {
   var findItems = [];
- for ( i = 0; i < items.length; i++) {
-  if (items[i] === "price") {
-    findItems.push((items[i]) / 0 )
+  for ( var count = 0; count < items.length; count++) {
+    if (items[count] !== 0) {
+      findItems.push(items[count].price)
   }
-}return findItems;
 }
+var avg = 0;
+for (var i = 0; i < findItems.length; i++) {
+  avg += findItems[i] / findItems.length
+}
+  return "The average price is " + avg.toFixed(2)
+};
 
-function avg (a, b, c)  {
-  return ((a + b + c) / 3)
+
+//Question 2
+
+function cost() {
+  var findCost = [];
+  for ( var count = 0; count < items.length; count++) {
+    if (items[count] >= 14 && items[count] <= 18) {
+      findItems.push(items[count].price)
+  }
 }
+var betweenItems = 0
+for (var i = 0; i < findCost.length; i++) {
+  betweenItems += findCost[i]
+
+  x >= 0    &&    x < 0.33
