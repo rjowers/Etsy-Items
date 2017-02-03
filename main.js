6bf6fb2e553720ff2eaadc23775b74df61f9a6ee
Ryan Jowers <rjowers@g.clemson.edu>
2017-02-02 20:08:12 -0500

    added header, body and footer stylings

---
+++ main.js
@@ -0,0 +1,14 @@
+
+
+function find() {
+  var findItems = [];
+ for ( i = 0; i < items.length; i++) {
+  if (items[i] === "price") {
+    findItems.push((items[i]) / 0 )
+  }
+}return findItems;
+}
+
+function avg (a, b, c)  {
+  return ((a + b + c) / 3)
+}
